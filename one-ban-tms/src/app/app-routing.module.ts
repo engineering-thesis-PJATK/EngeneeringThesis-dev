e0d9127d5f2e0a55d808ff410bae1008254ebba9
Jakub <kubhinio@gmail.com>
2022-03-17 16:46:11 +0100

    added user, sign-in, sign-out component

--- one-ban-tms/src/app/app-routing.module.ts
+++ one-ban-tms/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: UserComponent,
+  children:[{path:'',component:SignUpComponent}]},
+  { path: 'login', component: UserComponent,
+  children:[{path:'',component:SignInComponent}]},
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  //{ path: '', redirectTo: '/login', pathMatch: 'full'},
+  //{ path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- one-ban-tms/src/app/app.module.ts
+++ one-ban-tms/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    UserComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
