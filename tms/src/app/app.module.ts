984463609442e6c89f048183a0c69ba9e7df088d
S20034JM <s20034@pjwstk.edu.pl>
2022-03-20 14:26:21 +0100

    important fix
    import client module :)

--- tms/src/app/app.module.ts
+++ tms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,6 +42,7 @@ import { TicketListComponent } from './components/ticket/ticket-list/ticket-list
     BrowserAnimationsModule,
     FormsModule,
     MaterializeCollapsibleModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
