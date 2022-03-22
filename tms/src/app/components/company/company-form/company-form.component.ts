8681a1f7d3f8e569623c2c633332e85cf9093a76
S20034JM <s20034@pjwstk.edu.pl>
2022-03-22 22:48:54 +0100

    edited company form component

--- tms/src/app/components/company/company-form/company-form.component.ts
+++ tms/src/app/components/company/company-form/company-form.component.ts
@@ -1,5 +1,7 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Company } from 'src/app/models/company';
+import { CompanyAddress } from 'src/app/models/companyAddress';
 
 @Component({
   selector: 'app-company-form',
@@ -9,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 
 export class CompanyFormComponent implements OnInit {
 
+  company: Company = { companyName: '', nipPrefix: '', nip: '', landLine: '', companyAddresses: [] };
+
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onNewAddress(address: CompanyAddress) {
+    this.company.companyAddresses.push(address);
+  }
+
+  onDeleteAddress(address: CompanyAddress) {
+    this.company.companyAddresses = this.company.companyAddresses.filter(item => item !== address);
   }
 
 }
