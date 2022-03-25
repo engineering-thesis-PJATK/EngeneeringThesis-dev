3c4e93b7afa54f0f7425bc64935312f0a4fe7b7d
S20034JM <s20034@pjwstk.edu.pl>
2022-03-25 22:56:04 +0100

    modified company "list"

--- tms/src/app/components/company/company-card/company-card.component.ts
+++ tms/src/app/components/company/company-card/company-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CompanySimple } from 'src/app/models/companySimple';
 
 @Component({
   selector: 'app-company-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-card.component.scss']
 })
 export class CompanyCardComponent implements OnInit {
-
+  @Input() company: CompanySimple = {id: 0, companyName: '', city: '', country: ''};
   constructor() { }
 
   ngOnInit(): void {
--- tms/src/app/services/company/company.service.ts
+++ tms/src/app/services/company/company.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { Company } from 'src/app/models/company';
+import { CompanySimple } from 'src/app/models/companySimple';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class CompanyService {
   private url = 'http://localhost:3000';
   constructor(private http: HttpClient) {
 
+   }
+
+   getCompanies(): Observable<CompanySimple[]> {
+      return this.http.get<CompanySimple[]>(this.url+'/companies').pipe(tap(console.log));
    }
 
    postCompany(company: Company): Observable<string[]> {
