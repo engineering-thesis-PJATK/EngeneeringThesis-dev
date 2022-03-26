7b1828a9b5c45c8b47cad004cd8d7c47a434ebfd
S20034JM <s20034@pjwstk.edu.pl>
2022-03-26 10:27:19 +0100

    added privileges to init

--- tms/src/app/components/employee/employee-form/employee-form.component.ts
+++ tms/src/app/components/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeePrivilege } from 'src/app/models/employeePrivilege';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
 
 @Component({
   selector: 'app-employee-form',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-form.component.scss']
 })
 export class EmployeeFormComponent implements OnInit {
+  privilegeList!: Observable<EmployeePrivilege[]>;
 
-  constructor() { }
+  constructor(private http: EmployeeService) { }
 
   ngOnInit(): void {
+    this.privilegeList = this.http.getPriveleges();
   }
 
 }
