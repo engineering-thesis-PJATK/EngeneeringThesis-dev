42c0c395ed37efe8e0f95a2557b85f9c69ce0fde
S20034JM <s20034@pjwstk.edu.pl>
2022-03-26 10:24:01 +0100

    added global environment details

---
+++ tms/src/app/services/environment.ts
@@ -0,0 +1,11 @@
+export const Environment = {
+    baseUrl: 'http://localhost:3000',
+    production: false
+};
+
+export enum ApiPaths {
+    Login = '/login',
+    Company = '/companies',
+    Employee = '/employees',
+    EmployeePrivilege = '/employees/privileges'
+}
