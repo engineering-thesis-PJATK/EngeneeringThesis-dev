f970d439e4b3e6c299c138323acae2f12ebf4500
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2022-03-27 09:22:24 +0200

    Added array filling select with emps

--- tms/src/app/components/ticket/create-ticket/create-ticket.component.ts
+++ tms/src/app/components/ticket/create-ticket/create-ticket.component.ts
@@ -3,6 +3,11 @@ import $ from 'jquery'
 import { Datepicker } from "materialize-css";
 declare const M: any;
 
+interface Employee {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-create-ticket',
   templateUrl: './create-ticket.component.html',
@@ -12,6 +17,13 @@ declare const M: any;
 
 
 export class CreateTicketComponent implements OnInit {
+  employees: Employee[] = [
+    {value: '0', viewValue: 'Krzysztof Jurkowski'},
+    {value: '1', viewValue: 'Piotr Łojko'},
+    {value: '2', viewValue: 'Tomasz Krasieńko'},
+    {value: '3', viewValue: 'Jakub Michalak'},
+    {value: '4', viewValue: 'Paweł Lenkiewicz'}
+  ];
   
   constructor() {}
   ngOnInit(): void {}
