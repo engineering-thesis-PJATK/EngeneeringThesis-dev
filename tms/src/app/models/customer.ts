a69501123d01c24bf3ccbe14a1c15098ea9b183a
S20034JM <s20034@pjwstk.edu.pl>
2022-04-06 20:25:06 +0200

    added interface for sending customer

--- tms/src/app/models/customer.ts
+++ tms/src/app/models/customer.ts
@@ -21,3 +21,13 @@ export interface CustomerCompany {
     cur_idCompany: number;
     cmp_name: string;
 }
+
+export interface CustomerSend {
+    cur_name: string;
+    cur_surname: string;
+    cur_email: string;
+    cur_phoneNumber?: string;
+    cur_position: string;
+    cur_comments: string;
+    cur_idCompany: number;
+}
