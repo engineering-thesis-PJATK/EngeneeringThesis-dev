e5bfb6466fb133602aa30763c0a8d633881ef1da
S20034JM <s20034@pjwstk.edu.pl>
2022-04-11 21:20:39 +0200

    finished list form
    team List form is in team form

--- tms/src/app/components/team/team-form/team-member-list/team-member-list.component.ts
+++ tms/src/app/components/team/team-form/team-member-list/team-member-list.component.ts
@@ -1,12 +1,10 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CompanySelect } from 'src/app/models/company';
-import { TeamEmployee, Employee, TeamRole } from 'src/app/models/employee';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { TeamEmployee, Employee, TeamRole, TempMember } from 'src/app/models/employee';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
-//import { FormSelect } from 'materialize-css';
-import { CustomerSend } from 'src/app/models/customer';
-import { CompanyService } from 'src/app/services/company/company.service';
-//declare const M: any;
+import { NgModel } from '@angular/forms';
+import { Location } from '@angular/common';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-team-member-list',
@@ -15,12 +13,14 @@ import { CompanyService } from 'src/app/services/company/company.service';
 })
 export class TeamMemberListComponent implements OnInit, AfterViewInit {
   @Input() members!: TeamEmployee[];
-  @Output() removeMember = new EventEmitter<TeamEmployee>();
+  //@Output() removeMember = new EventEmitter<TeamEmployee>();
+  //@Output() addMember = new EventEmitter<TeamEmployee>();
   
   employees: Partial<Employee[]> = [];
   roles: Partial<TeamRole[]> = [];
+  sendingEmployee: Partial<TempMember> = {};
 
-  constructor(private empHttp: EmployeeService) { }
+  constructor(private empHttp: EmployeeService,private location: Location) { }
 
   ngOnInit(): void {
     this.empHttp.getEmployees().subscribe(emp => this.employees = emp);
@@ -28,28 +28,67 @@ export class TeamMemberListComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    
     this.loadSelect();
-    this.loadAuto();
-   
   }
 
-  loadAuto(): void {
-    var elems = document.querySelectorAll('.autocomplete');
-    let empMap: {[key: string]: string | null} = {};
-    this.employees.forEach(emp => {
-      empMap[emp?.empName+' '+emp?.empSurname] = null;
-    });
-    var options = {
-      data: empMap
-    }
-    var instances = M.Autocomplete.init(elems, options);
-  }
+  // loadAuto(): void {
+  //   var elems = document.querySelectorAll('.autocomplete');
+  //   let empMap: {[key: string]: string | null} = {};
+  //   this.employees.forEach(emp => {
+  //     empMap[emp?.empName+' '+emp?.empSurname] = null;
+  //   });
+  //   var options = {
+  //     data: empMap
+  //   }
+  //   this.instances = M.Autocomplete.init(elems, options);
+  // }
 
   loadSelect(): void {
     var elems2 = document.querySelectorAll('select');
     var instances2 = M.FormSelect.init(elems2, {});
   }
 
-  
+  removeMember(member: TeamEmployee) {
+    this.members = this.members.filter(item => item != member);
+  }
+
+  addMember(member: Partial<TempMember>) {
+    var emp =  this.employees.find(item => item?.empId == member?.empId);
+    var role = this.roles.find(item => item?.etrId == member.empRole);
+    if(emp === undefined || role === undefined)
+    {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You have to choose employee and role!',
+        // footer: '<a href="">Why do I have this issue?</a>'
+      });
+      return;
+    }
+
+    var tmpMember = this.members.find(x => x.empId == member.empId);
+    if(tmpMember !== undefined)
+    {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Oops...',
+        text: 'You have already added this employee!',
+        // footer: '<a href="">Why do I have this issue?</a>'
+      });
+      return;
+    }
+
+    let tempEmp: TeamEmployee = {
+      empId: emp?.empId,
+      empName: emp?.empName,
+      empSurname: emp?.empSurname,
+      etrId: 0,
+      empRole: role
+    };
+    if(!this.members)
+    {
+      this.members = [];
+    }
+    this.members.push(tempEmp);
+  }
 }
