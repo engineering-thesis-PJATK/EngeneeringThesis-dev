cc5d52591826c6187ac4a34750254ea18717978f
S20034JM <s20034@pjwstk.edu.pl>
2022-04-22 22:06:11 +0200

    not important formatting code

--- tms/src/app/components/employee/employee-form/employee-form.component.ts
+++ tms/src/app/components/employee/employee-form/employee-form.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeePrivilege } from 'src/app/models/employeePrivilege';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
-import { Location } from '@angular/common'
+import { Location } from '@angular/common';
 import { FormSelect } from 'materialize-css';
 import { EmployeeSend } from 'src/app/models/employee';
 import { NgModel } from '@angular/forms';
@@ -11,46 +11,42 @@ declare const M: any;
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
-  styleUrls: ['./employee-form.component.scss']
+  styleUrls: ['./employee-form.component.scss'],
 })
 export class EmployeeFormComponent implements OnInit, AfterViewInit {
   employee: Partial<EmployeeSend> = {};
 
   privilegeList: EmployeePrivilege[] = [];
 
-  constructor(private http: EmployeeService, private location: Location) { }
+  constructor(private http: EmployeeService, private location: Location) {}
   ngAfterViewInit(): void {
     var options = {
-  
-      isMultiple:true,
-      }
-      var elems = document.querySelectorAll('select');
-      var instances = M.FormSelect.init(elems, options);
+      isMultiple: true,
+    };
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
   }
 
   ngOnInit(): void {
     //this.privilegeList = this.http.getPriveleges();
-    this.http.getPriveleges().subscribe(prv => this.privilegeList = prv);
+    this.http.getPriveleges().subscribe((prv) => (this.privilegeList = prv));
   }
 
   returnButtonClick() {
     this.location.back();
   }
 
-  addEmployee()
-  {
+  addEmployee() {
     console.log(this.employee);
   }
 
-  print(any: NgModel)
-  {
+  print(any: NgModel) {
     console.log(any);
   }
-
 }
 
 //regexy na hasła
-//conajmniej 1 mała litera, conajmniej 0 dużych, 1 cyfra i jeden znak specjalny 
+//conajmniej 1 mała litera, conajmniej 0 dużych, 1 cyfra i jeden znak specjalny
 //^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$
 
 //1+ mała, 1+ duża, 1+ cyfra, 1+ znak
