49d3800b5939448bac9eb625aea1f4244b560327
S20034JM <s20034@pjwstk.edu.pl>
2022-04-23 21:27:36 +0200

    update project service - added method
    method returns project task object based on id

--- tms/src/app/services/project/project.service.ts
+++ tms/src/app/services/project/project.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
-import { Project, ProjectSending } from 'src/app/models/project';
+import { Project, ProjectSending, ProjectTask } from 'src/app/models/project';
 import { ApiPaths, Environment } from '../environment';
 
 @Injectable({
@@ -26,8 +26,22 @@ export class ProjectService {
   }
 
   getProject(id: string): Observable<Project> {
-    //return this.http.get(this.url+ApiPaths.Project+'/'+id).pipe(tap(console.log));
+    //return this.http.get(this.url+ApiPaths.ProjectTasks+'/'+id).pipe(tap(console.log));
     let project: Project =  { prjId: 1, prjName: 'first project', prjDescription: 'description dfjlkfdjfksfdsfdsfsfdsf  sdf sdf', companyId: 1, companyName: 'first compnay', teamId: 1, teamName: 'abc team name'};
     return of(project);
+  }
+
+  getTasks(id: string): Observable<ProjectTask[]> {
+    //return this.http.get(this.url+ApiPaths.ProjectTasks+'/'+id).pipe(tap(console.log));
+    let tasks: ProjectTask[] = [
+      { ptId: 1, ptEmpId: 1, ptEmpName: 'Jan Kowalski', ptContent: 'sprawdzić logi', ptEstimatedCost: 21.32},
+      { ptId: 1, ptEmpId: 2, ptEmpName: 'Tomasz Nowak', ptContent: 'Integracja systemów x y', ptEstimatedCost: 221.32},
+    ];
+    return of(tasks);
+  }
+
+  putTask(task: ProjectTask): Observable<string> {
+    //return this.http.put(this.url+ApiPaths.ProjectTasks, task).pipe(tap(console.log));
+    return of('ok');
   }
 }
