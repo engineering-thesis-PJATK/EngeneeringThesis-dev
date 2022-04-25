4aa8a9d8f9463fe8cba7f1de94eefa93ec89662c
S20034JM <s20034@pjwstk.edu.pl>
2022-04-25 22:00:48 +0200

    update project task
    added 'sidebars' for edit task and insert time. Time side is not finished yet.

--- tms/src/app/components/project/project-details/project-task/project-task.component.ts
+++ tms/src/app/components/project/project-details/project-task/project-task.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 import { Observable, switchMap } from 'rxjs';
 import { ProjectTask } from 'src/app/models/project';
@@ -13,22 +13,36 @@ import { EmployeeService } from 'src/app/services/employee/employee.service';
 })
 export class ProjectTaskComponent implements OnInit, AfterViewInit {
   @Input() projectId!: number;
-  //@Input() tasks!: ProjectTask[];
-  //@Output() newTask = new EventEmitter<ProjectTask>();
+  @ViewChild('editDescription') editDescription!: ElementRef;
+  @ViewChild('editEmp') editEmp!: ElementRef;
+  @ViewChild('ToDo') ToDo!: ElementRef;
+  @ViewChild('InProgress') InProgress!: ElementRef;
+  @ViewChild('Finished') Finished!: ElementRef;
 
   employees!: Observable<Employee[]>;
   tasks!: Observable<ProjectTask[]>;
 
-  sendingTask: ProjectTask = { ptId: this.projectId, ptEmpId: 0, ptEmpName: '', ptContent: '', ptEstimatedCost: 0};
+  sendingTask: ProjectTask = { ptId: this.projectId, ptEmpId: 0, ptEmpName: '', ptContent: '', ptEstimatedCost: 0, ptState: 'ToDo'};
+  editedTask!: ProjectTask;
   constructor(private projectHttp: ProjectService, private route: ActivatedRoute, private empHttp: EmployeeService) { }
 
 
   ngAfterViewInit(): void {
+    this.initCollapsible();
+    this.initDP();
+  }
+
+  initCollapsible() {
     var elems = document.querySelectorAll('.collapsible');
     var options = {
       accordion: false
     }
     var instances = M.Collapsible.init(elems, options);
+  }
+
+  initDP() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {});
   }
 
   ngOnInit(): void {
@@ -38,11 +52,32 @@ export class ProjectTaskComponent implements OnInit, AfterViewInit {
     );
   }
 
-  // TODO: metoda dodająca task - jeżeli się powiedzie to odświeżenie sql lub push do lokalnej listy??
   addTask(): void {
     let response = this.projectHttp.putTask(this.sendingTask);
     this.getTasks();
-    console.log('done');
+  }
+
+  setEditedTask(task: ProjectTask): void {
+    this.editedTask = task;
+    console.log(this.editedTask);
+    console.log(this.ToDo);
+    if(this.editedTask.ptState == 'ToDo') {
+      this.ToDo.nativeElement.checked = true;
+    }
+    else if (this.editedTask.ptState == 'In progress') {
+      this.InProgress.nativeElement.checked = true;
+    }
+    else if (this.editedTask.ptState == 'Finished') {
+      this.Finished.nativeElement.checked = true;
+    }
+    this.editDescription.nativeElement.value = this.editedTask.ptContent;
+    this.editEmp.nativeElement.value = this.editedTask.ptEmpName;
+  }
+
+  setTaskFinished(task: ProjectTask): void {
+    let response = this.projectHttp.postTask(task);
+    //task.ptState = 'Finished';
+    this.getTasks();
   }
 
   getTasks(): void {
@@ -50,4 +85,6 @@ export class ProjectTaskComponent implements OnInit, AfterViewInit {
       switchMap((params: ParamMap) => this.projectHttp.getTasks(params.get('id') || '0'))
     );
   }
+
+  addTime(){}//TODO : dokończyć sidenava, następnie dodać metodę i walidację na wprowadzany czas.
 }
