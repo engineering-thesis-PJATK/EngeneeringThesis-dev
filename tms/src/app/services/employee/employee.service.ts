40e4d32a8eaf35cb1986ef456111454f80c1a93d
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-05-10 21:37:37 +0200

    added required methods
    Methods are not implemented yet

--- tms/src/app/services/employee/employee.service.ts
+++ tms/src/app/services/employee/employee.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
 import { EmployeePrivilege } from '../../models/employeePrivilege';
-import { Employee, EmployeeEdit, EmployeeSend, TeamRole } from '../../models/employee';
+import { Employee, EmployeeEdit, EmployeeSend, TeamEmployee, TeamRole } from '../../models/employee';
 import { Environment } from '../environment';
 import { ApiPaths } from '../environment';
 
@@ -70,6 +70,28 @@ export class EmployeeService {
       { empId: 3, empName: 'Barbara Squirrel', empPhoneNumber: '666264362', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
     ];
     return of(cmps);
+  }
+
+  getTeamMembers(id: string): Observable<TeamEmployee[]> {
+    //return this.http.get(this.url+ApiPaths.Employee+'/project/'+id).pipe(tap(console.log));
+    let cmps: TeamEmployee[] = [
+      { empId: 1, empName: 'Jan', empSurname:'Kowalski', empRole: {etrId: 1, etrName: 'TeamLeader' } },
+      { empId: 2, empName: 'Jonh X', empSurname:'Kowalski', empRole: {etrId: 3, etrName: 'BackendDev' }   },
+      { empId: 3, empName: 'Barbara Squirrel', empSurname:'Kowalski', empRole: {etrId: 3, etrName: 'BackendDev' }   },
+    ];
+    return of(cmps);
+  }
+
+  putTeamMember(employee: TeamEmployee, teamId: number){
+    //TODO : przygotować http put
+  }
+
+  postTeamMember(employee: TeamEmployee, teamId: number){
+    //TODO : przygotować http post
+  }
+
+  deleteTeamMember(employee: TeamEmployee, teamId: number){
+    //TODO : przygotować http delete
   }
 
   postEmployee(employee: EmployeeSend) {
