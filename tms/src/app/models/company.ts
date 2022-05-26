542fdeecb323a8bf610b5031d0bfadc668db7e44
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-05-26 20:58:49 +0200

    company card interface update

--- tms/src/app/models/company.ts
+++ tms/src/app/models/company.ts
@@ -1,16 +1,27 @@
 import { CompanyAddress } from "./companyAddress";
 
 export interface CompanyCard {
-    cmpId?: number;
-    cmpName: string;
-    cmpNip?: string;
-    cmpNipPrefix?: string;
-    cmpRegon?: string;
-    cmpKrsNumber?: string;
-    cmpLandLine?: string;
-    cmpIdAddress?: number;
-    cmpCity: string;
-    cmpCountry: string;
+    // cmpId?: number;
+    // cmpName: string;
+    // cmpNip?: string;
+    // cmpNipPrefix?: string;
+    // cmpRegon?: string;
+    // cmpKrsNumber?: string;
+    // cmpLandLine?: string;
+    // cmpIdAddress?: number;
+    // cmpCity: string;
+    // cmpCountry: string;
+    cmpId?: number,
+    cmpName: string,
+    cmpNip?: string,
+    cmpNipPrefix?: string,
+    cmpRegon?: string,
+    cmpKrsNumber?: string,
+    cmpLandline?: string,
+    addresses?: [],
+    companyNotes?: [],
+    customers?: [],
+    projects?: []
 }
 
 export interface CompanySend {
