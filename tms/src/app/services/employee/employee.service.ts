c6f1b7eb668e384dbc93693bbdc8be6f115de3ec
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-05-26 22:37:59 +0200

    update employee service
    connecting with web api instead static data

--- tms/src/app/services/employee/employee.service.ts
+++ tms/src/app/services/employee/employee.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
-import { EmployeePrivilege } from '../../models/employeePrivilege';
-import { Employee, EmployeeEdit, EmployeeSend, TeamEmployee, TeamRole } from '../../models/employee';
+import { catchError, delay, first, last, Observable, of, take, tap } from 'rxjs';
+import { EmployeeNew, EmployeePrivilege } from '../../models/employee';
+import { Employee,  EmployeeEdit, TeamEmployee, TeamRole } from '../../models/employee';
 import { Environment } from '../environment';
 import { ApiPaths } from '../environment';
+import { HandlerService } from '../handler/handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,43 +14,24 @@ export class EmployeeService {
 
 
   private url = Environment.baseUrl;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private handler: HandlerService) { }
 
   getEmployees(): Observable<Employee[]> {
-   // return this.http.get<Employee[]>(this.url+ApiPaths.Employee).pipe(tap(console.log));
-    
-    let cmps: Employee[] = [
-      { empId: 1, empName: 'Jan', empPhoneNumber: '223441425', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski' },
-      { empId: 2, empName: 'Jonh X', empPhoneNumber: '456456234', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
-      { empId: 3, empName: 'Barbara Squirrel', empPhoneNumber: '666264362', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
-    ];
-    return of(cmps);
+    return this.http.get<Employee[]>(this.url+ApiPaths.Employee).pipe();
   }
 
-  getEmployee(id: string): Observable<EmployeeEdit> {
-    // return this.http.get<Employee>(this.url+ApiPaths.Employee+'/'+id).pipe(tap(console.log));
-     
-     let emp: EmployeeEdit = {
-       empId: 1, empName: 'Jan', empPhoneNumber: '223441425', empEmail: 'Poland', empLogin: 'x', empSurname: 'Kowalski',
-       empPrivileges: [{ id: 1, name: 'User'}]
-     };
-     return of(emp);
-   }
-
-   getEmployeePrivileges(id: string): Observable<EmployeePrivilege[]> {
-    let privileges: EmployeePrivilege[] = [
-      { id: 1, name: 'User'},
-    ]
-    return of(privileges);
+  getEmployee(id: string): Observable<Employee> {
+    return this.http.get<Employee>(this.url+ApiPaths.Employee+'/'+id).pipe();
    }
 
   getPriveleges(): Observable<EmployeePrivilege[]> {
-    let privileges: EmployeePrivilege[] = [
-      { id: 1, name: 'User'},
-      { id: 2, name: 'Admin'},
-    ]
-    return of(privileges);
-    //return this.http.get<EmployeePrivilege[]>(this.url+ApiPaths.EmployeePrivilege).pipe(tap(console.log));
+    return this.http.get<EmployeePrivilege[]>(this.url+ApiPaths.EmployeePrivilege).pipe(tap(console.log));
+  }
+
+  getForgotPassword(email : string) {
+    email = email.replace("@","%40");
+    console.log(email);
+    return this.http.get<any>(this.url+ApiPaths.ForgotPassword+`?emailAddress=${email}`).pipe(tap(console.log));
   }
 
   getRoles(): Observable<TeamRole[]> {
@@ -63,13 +45,13 @@ export class EmployeeService {
   }
 
   getTeamEmployees(id: string): Observable<Employee[]> {
-    //return this.http.get(this.url+ApiPaths.Employee+'/project/'+id).pipe(tap(console.log));
-    let cmps: Employee[] = [
-      { empId: 1, empName: 'Jan', empPhoneNumber: '223441425', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski' },
-      { empId: 2, empName: 'Jonh X', empPhoneNumber: '456456234', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
-      { empId: 3, empName: 'Barbara Squirrel', empPhoneNumber: '666264362', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
-    ];
-    return of(cmps);
+    return this.http.get(this.url+ApiPaths.Employee+'/project/'+id).pipe(tap(console.log));
+    // let cmps: Employee[] = [
+    //   { empId: 1, empName: 'Jan', empPhoneNumber: '223441425', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski' },
+    //   { empId: 2, empName: 'Jonh X', empPhoneNumber: '456456234', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
+    //   { empId: 3, empName: 'Barbara Squirrel', empPhoneNumber: '666264362', empEmail: 'Poland',empLogin:'x',empSurname:'Kowalski'  },
+    // ];
+    // return of(cmps);
   }
 
   getTeamMembers(id: string): Observable<TeamEmployee[]> {
@@ -94,12 +76,16 @@ export class EmployeeService {
     //TODO : przygotować http delete
   }
 
-  postEmployee(employee: EmployeeSend) {
-    return this.http.post<string[]>(this.url+ApiPaths.Employee,employee).pipe(tap(console.log));
+  postEmployee(employee: EmployeeNew) {
+    return this.http.post<any>(this.url+ApiPaths.Employee,employee).pipe(tap(console.log),catchError(this.handler.handleError));
   }
 
-  putEmployee(employee: EmployeeEdit) {
-    return this.http.put<string[]>(this.url+ApiPaths.Employee,employee).pipe(tap(console.log));
+  postEmployeePrivileges(id: number, privileges: number[]) {
+    return this.http.post<any>(this.url+ApiPaths.Employee+`/${id}/roles`,privileges).pipe(tap(console.log));
+  }
+
+  putEmployee(id: number, employee: EmployeeEdit) {
+    return this.http.put<any>(this.url+ApiPaths.Employee+`/${id}`,employee).pipe(tap(console.log));
   }
 
   deleteEmployee(id: number): string {
