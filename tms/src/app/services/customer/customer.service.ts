1c3656e0a1f2b075663eb234e1ed222eb2d71cda
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-06-16 21:42:10 +0200

    update customer service - removed static data, add true api

--- tms/src/app/services/customer/customer.service.ts
+++ tms/src/app/services/customer/customer.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Customer, CustomerCompany, CustomerSend } from 'src/app/models/customer';
 import { Environment } from '../environment';
 import { ApiPaths } from '../environment';
+import { HandlerService } from '../handler/handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,35 +12,25 @@ import { ApiPaths } from '../environment';
 export class CustomerService {
 
   private url = Environment.baseUrl;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private handler: HandlerService) {}
 
   getCustomers(): Observable<CustomerCompany[]> {  
-    //return this.http.get<Customer[]>(this.url+ApiPaths.Customer).pipe(tap(console.log));
-    let cus: CustomerCompany[] = [
-      {cur_id: 1, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:1, cmp_name: 'FirmaX', cur_phoneNumber:'344239842'},
-      {cur_id: 2, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:1, cmp_name: 'FirmaX', cur_phoneNumber:'344239842'},
-      {cur_id: 3, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:2, cmp_name: 'FirmaZ', cur_phoneNumber:'344239842'},
-      {cur_id: 4, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:2, cmp_name: 'FirmaZ', cur_phoneNumber:'344239842'},
-      {cur_id: 5, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:2, cmp_name: 'FirmaZ', cur_phoneNumber:'344239842'},
-      {cur_id: 6, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:3, cmp_name: 'FirmaD', cur_phoneNumber:'344239842'},
-      {cur_id: 7, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:3, cmp_name: 'FirmaD', cur_phoneNumber:'344239842'},
-      {cur_id: 8, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:4, cmp_name: 'Firmawerw', cur_phoneNumber:'344239842'},
-    ]
-    return of(cus);
+    return this.http.get<CustomerCompany[]>(this.url+ApiPaths.CustomerCompanyName).pipe(tap(console.log),catchError(this.handler.handleError));
   }
 
-  getCustomer(id: string): Observable<Partial<Customer>> {
-    //return this.http.get<Partial<Customer>>(this.url+ApiPaths.Customer+'/'+id).pipe(tap(console.log));
-    let customer: Partial<Customer> = {cur_id: 1, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:1, cmp_name: 'FirmaX', cur_phoneNumber:'344239842'}
-    return of(customer);
+  getCustomer(id: string): Observable<CustomerCompany> {
+    return this.http.get<CustomerCompany>(this.url+ApiPaths.CustomerCompanyName+'/'+id).pipe(tap(console.log));
+    //let customer: Partial<Customer> = {cur_id: 1, cur_name: 'Adam',cur_surname:'Kowalski', cur_email:'ddd', cur_position:"xxx", cur_comments:'hkdsjhskjfhskjfhdfsdfsdff', cur_idCompany:1, cmp_name: 'FirmaX', cur_phoneNumber:'344239842'}
+    //return of(customer);
   }
 
-  putCustomer(customer: Partial<Customer>) {
-    //return this.http.put<object>(this.url+ApiPaths.Customer,customer).pipe(tap(console.log));
-
+  putCustomer(customer: CustomerSend, customerId: number) {
+    return this.http.put<any>(this.url+ApiPaths.Customer+'/'+customerId,customer).pipe(tap(console.log),catchError(this.handler.handleError));
   }
 
-  postCustomer(customer: CustomerSend) {
+  postCustomer(customer: CustomerSend, companyId: number) {
+    ///api/Customer/{companyId}
+    return this.http.post<any>(this.url+ApiPaths.Customer+`/${companyId}`,customer).pipe(tap(console.log),catchError(this.handler.handleError));
     //return this.http.post<object>(this.url+ApiPaths.Customer,customer).pipe(tap(console.log));
   }
 
