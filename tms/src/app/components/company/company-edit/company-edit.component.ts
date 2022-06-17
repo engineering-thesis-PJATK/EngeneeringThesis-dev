9844b55f27938981efb1fe2b64b1d7d6a0fb40d9
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-06-17 12:04:52 +0200

    created company edit component
    NOT FINISHED

---
+++ tms/src/app/components/company/company-edit/company-edit.component.ts
@@ -0,0 +1,85 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Company } from 'src/app/models/company';
+import { CompanyAddress } from 'src/app/models/companyAddress';
+import { CompanyService } from 'src/app/services/company/company.service';
+import { Location } from '@angular/common';
+declare const M: any;
+
+@Component({
+  selector: 'app-company-edit',
+  templateUrl: './company-edit.component.html',
+  styleUrls: ['./company-edit.component.scss']
+})
+export class CompanyEditComponent implements OnInit, AfterViewInit {
+
+  company!: Company;
+  addresses!: CompanyAddress[];
+  editedAddress!: CompanyAddress;
+  firstEdit = true;
+  constructor(private http: CompanyService, private location: Location, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.http.getCompany(this.route.snapshot.paramMap.get('id') || '0').subscribe((cmp) => (this.company = cmp));
+    this.getAddresses();
+  }
+
+  ngAfterViewInit(): void {
+    this.initCollapsible();
+  }
+
+  initCollapsible() {
+    var elems = document.querySelectorAll('.collapsible');
+    var options = {
+      inDuration: 500,
+      outDuration: 500
+    }
+    var instances = M.Collapsible.init(elems, options);
+  }
+
+  returnButtonClick() {
+    this.location.back();
+  }
+
+  updateCompany() {
+
+  }
+
+  removeAddress(address: CompanyAddress) {
+    //http remove address
+  }
+
+  setAddress(address: CompanyAddress) {
+    //http update address
+    var elem = document.querySelector('.emp-edit');
+    var instance = M.Collapsible.getInstance(elem);
+    instance.close(0);
+    if(this.firstEdit) {
+      instance.open(0);
+      this.editedAddress = address;
+    } else {
+      setTimeout(() => 
+      {
+        instance.open(0);
+        this.editedAddress = address;
+      },
+      500);
+    }
+    
+    this.firstEdit = false;
+  }
+
+  onNewAddress(address: CompanyAddress) {
+    //http.addAddress
+    this.addresses.push(address);
+  }
+
+  onUpdateAddress(address: CompanyAddress) {
+    
+  }
+
+  getAddresses() {
+    this.http.getCompanyAddresses(this.route.snapshot.paramMap.get('id') || '0').subscribe((adr) => (this.addresses = adr));
+  }
+
+}
