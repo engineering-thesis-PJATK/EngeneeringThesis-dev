4a346296a3da45b8f6808ea4e040f4b760344028
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-07-08 21:27:04 +0200

    enable web api to getter

--- tms/src/app/services/team/team.service.ts
+++ tms/src/app/services/team/team.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
 import { Team } from 'src/app/models/team';
@@ -12,19 +12,20 @@ export class TeamService {
   private url = Environment.baseUrl;
   constructor(private http: HttpClient) { }
 
-  postTeam(team: Team) {
-    return this.http.post<any>(this.url+ApiPaths.Team,team).pipe(tap(console.log));
+  postTeam(teamName : string) {
+    //"https://localhost:44330/api/Employee/Team?TemName=dupa%20%23%241"
+    return this.http.post<any>(this.url+ApiPaths.Team+`?TemName=${encodeURIComponent(teamName)}`,{}).pipe(tap(console.log));
   }
 
   getTeams(): Observable<Team[]> {
-    //return this.http.get<TeamSimple[]>(this.url+ApiPaths.Team).pipe(tap(console.log));
-    let teas: Team[] = [
-      {temId: 1, temName: 'Team 1'},
-      {temId: 2, temName: '.NET Team'},
-      {temId: 3, temName: 'bla bla'},
-      {temId: 4, temName: 'test123'},
-    ];
-    return of(teas);
+    return this.http.get<Team[]>(this.url+ApiPaths.Team).pipe(tap(console.log));
+    // let teas: Team[] = [
+    //   {temId: 1, temName: 'Team 1'},
+    //   {temId: 2, temName: '.NET Team'},
+    //   {temId: 3, temName: 'bla bla'},
+    //   {temId: 4, temName: 'test123'},
+    // ];
+    // return of(teas);
   }
 
   getTeam(temId: string): Observable<Team> {
