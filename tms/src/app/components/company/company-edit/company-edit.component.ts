d34cb2e8e473364764e0322ae81cb40057d02664
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-07-11 22:34:08 +0200

    connect company update with api

--- tms/src/app/components/company/company-edit/company-edit.component.ts
+++ tms/src/app/components/company/company-edit/company-edit.component.ts
@@ -1,8 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Company } from 'src/app/models/company';
+import { Company, CompanySend } from 'src/app/models/company';
 import { CompanyService } from 'src/app/services/company/company.service';
 import { Location } from '@angular/common';
+import { map } from 'rxjs';
 declare const M: any;
 
 @Component({
@@ -23,6 +24,12 @@ export class CompanyEditComponent implements OnInit {
   }
 
   updateCompany() {
-
+    this.http.putCompany(this.company.cmpId || 0, this.company as CompanySend).pipe(
+      map(res => {
+        if(res.statusCode == 200) {
+          this.returnButtonClick();
+        }
+      })
+    ).subscribe();
   }
 }
