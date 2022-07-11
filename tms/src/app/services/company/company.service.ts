aa52baa5d853f29ce5bd62a578c8017a98276cfd
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-07-11 22:35:10 +0200

    created some endponts

--- tms/src/app/services/company/company.service.ts
+++ tms/src/app/services/company/company.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, of, tap } from 'rxjs';
-import { Company,CompanySelect,CompanySend } from 'src/app/models/company';
+import { Company,CompanySend } from 'src/app/models/company';
 import { CompanyAddress } from 'src/app/models/companyAddress';
 import { Environment } from '../environment';
 import { ApiPaths } from '../environment';
@@ -18,13 +18,7 @@ export class CompanyService {
    }
 
    getCompanies(): Observable<Company[]> {
-      // return this.http.get<CompanyCard[]>(this.url+ApiPaths.Company,{observe: 'response'}).pipe(map(data => {
-        
-      //   //console.log("Here will be return response code Ex :200", data.status)
-      //   //return data.status
-      //   console.log(data);
-      // }), catchError(this.handler.handleError));
-      return this.http.get<Company[]>(this.url+ApiPaths.Company).pipe(catchError(this.handler.handleError));///*map(data=>{}),*/catchError(this.handler.handleError));
+      return this.http.get<Company[]>(this.url+ApiPaths.Company).pipe(catchError(this.handler.handleError));
    }
 
    getCompany(id: string): Observable<Company> {
@@ -39,6 +33,14 @@ export class CompanyService {
      return this.http.post<any>(this.url+ApiPaths.Company,company).pipe(tap(console.log),catchError(this.handler.handleError));
    }
 
+   putCompany(companyId: number, company: CompanySend): Observable<any> {
+    return this.http.put<any>(this.url+ApiPaths.Company+'/'+companyId,company).pipe(tap(console.log),catchError(this.handler.handleError));
+   }
+
+   deleteCompany(companyId: number) {
+    return this.http.delete<any>(this.url+ApiPaths.Company+'/'+companyId).pipe(tap(console.log),catchError(this.handler.handleError));
+   }
+
    postCompanyAddress(companyId: number, address: CompanyAddress): Observable<any> {
     return this.http.post<any>(this.url+ApiPaths.Company+'/'+companyId+ApiPaths.Address,address).pipe(tap(console.log),catchError(this.handler.handleError));
    }
@@ -51,13 +53,13 @@ export class CompanyService {
     return this.http.delete<any>(this.url+ApiPaths.Company+ApiPaths.Address+'/'+addressId).pipe(tap(console.log),catchError(this.handler.handleError));
    }
 
-   getCompaniesSelect(): Observable<CompanySelect[]> {
-     //return this.http.get<CompanySelect[]>(this.url+ApiPaths.CustomerSelect).pipe(tap(console.log));
-     let cmps: CompanySelect[] = [
-       {cmpId: 1, cmpName: 'first company'},
-       {cmpId: 2, cmpName: 'sec company'},
-       {cmpId: 3, cmpName: 'third company'}
-     ];
-     return of(cmps);
+   getCompaniesSelect(): Observable<Company[]> {
+     return this.http.get<Company[]>(this.url+ApiPaths.Company).pipe(tap(console.log),catchError(this.handler.handleError));
+    //  let cmps: CompanySelect[] = [
+    //    {cmpId: 1, cmpName: 'first company'},
+    //    {cmpId: 2, cmpName: 'sec company'},
+    //    {cmpId: 3, cmpName: 'third company'}
+    //  ];
+    //  return of(cmps);
    }
 }
