9450f51436044945121f4fc2c67bcbbdd84e01d1
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-07-11 22:32:03 +0200

    added button for deleting company

--- tms/src/app/components/company/company-card/company-card.component.ts
+++ tms/src/app/components/company/company-card/company-card.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { map } from 'rxjs';
 import { Company } from 'src/app/models/company';
+import { CompanyService } from 'src/app/services/company/company.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-company-card',
@@ -8,9 +11,39 @@ import { Company } from 'src/app/models/company';
 })
 export class CompanyCardComponent implements OnInit {
   @Input() company!: Company;
-  constructor() { }
+  @Output() getCompanies = new EventEmitter();
+  constructor(private http: CompanyService) { }
 
   ngOnInit(): void {
+  }
+
+  deleteCompany()
+  {
+    Swal.fire({
+      title: 'Do you want to delete a company?',
+      showDenyButton: true,
+      confirmButtonText: `Yes`,
+      denyButtonText: `No`,
+      icon: 'warning'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.handleDelete();
+      } 
+      // else if (result.isDenied) {
+      //   // ...
+      // }
+    })
+    
+  }
+
+  handleDelete() {
+    this.http.deleteCompany(this.company.cmpId || 0).pipe(
+      map(res => {
+        if(res.statusCode = 200) {
+          this.getCompanies.emit();
+        }
+      })
+    ).subscribe();
   }
 
 }
